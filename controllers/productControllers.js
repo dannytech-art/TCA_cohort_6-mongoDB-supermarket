a3cfb47a8d576fedf08dbebfc526d24e6f2caf92
Daniel obinna <dobi44134@gmail.com>
2025-09-08 00:54:27 +0100

    update

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -49,3 +49,49 @@ exports.getOneProduct = async (req,res)=>{
         })
     }
 }
+exports.updateProduct = async (req,res) =>{
+   try {
+        const {id} = req.params
+        const update = req.body
+        const product = await productModel.findById(id)
+
+        if (!product) {
+            return res.status(404).json({
+                message:` product not found `
+            })
+        }
+
+        const updated = await productModel.findByIdAndUpdate( id, update, {new: true} )
+        res.status(201).json({
+            message: `successfuly updated`,
+            data: updated
+        })
+   } catch (error) {
+        res.status(500).json({
+            message: `error fetching products`,
+            error: error.message
+        })
+   }
+}
+exports.deleteProduct = async (req,res)=>{
+         try {
+            const {id} = req.params
+            const product = await productModel.findById(id)
+
+        if (!product) {
+            return res.status(404).json({
+                message:` product not found `
+            })
+        }
+            const deleteProduct = await productModel.findByIdAndDelete(id)
+            res.status(200).json({
+                message:`deleted successfully`,
+                data: deleteProduct
+            })
+         } catch (error) {
+        res.status(500).json({
+            message: `error fetching products`,
+            error: error.message
+        })
+   }
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 
-const { addProducts, getAllProducts, getOneProduct } = require('../controllers/productControllers')
+const { addProducts, getAllProducts, getOneProduct, updateProduct, deleteProduct } = require('../controllers/productControllers')
 
 router.post('/product', addProducts)
 router.get('/products', getAllProducts)
 router.get('/product/:id', getOneProduct)
+router.patch('/product/:id', updateProduct)
+router.delete('/product/:id', deleteProduct)
 
 module.exports = router
